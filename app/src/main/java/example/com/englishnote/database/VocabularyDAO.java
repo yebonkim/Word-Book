992bc9e36cde22a8c6ec3fcd856696e0d7110621
Yebonkim <v954242@gmail.com>
2019-12-31 20:17:37 +0900

    Add Room DB Component

---
+++ app/src/main/java/example/com/englishnote/database/VocabularyDAO.java
@@ -0,0 +1,31 @@
+package example.com.englishnote.database;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+import example.com.englishnote.model.Vocabulary;
+import io.reactivex.Flowable;
+import io.reactivex.Observable;
+import io.reactivex.Single;
+
+@Dao
+public interface VocabularyDAO {
+    @Insert
+    Single<Long> insert(Vocabulary voca);
+
+    @Update
+    Single<Integer> update(Vocabulary voca);
+
+    @Query("SELECT * FROM vocabulary")
+    Observable<List<Vocabulary>> selectAll();
+
+    @Query("SELECT * FROM vocabulary WHERE id = :id")
+    Observable<Vocabulary> findById(int id);
+
+    @Query("SELECT COUNT(*) FROM vocabulary")
+    Observable<Integer> getCount();
+}
--- app/src/main/java/example/com/englishnote/model/Vocabulary.java
+++ app/src/main/java/example/com/englishnote/model/Vocabulary.java
@@ -1,54 +1,63 @@
 package example.com.englishnote.model;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
 /**
  * Created by yebonkim on 2018. 3. 20..
  */
 
+@Entity
 public class Vocabulary {
-    private int mId;
-    private String mEnglish;
-    private String mMeans;
-    private long mTimestamp;
+    @PrimaryKey
+    private int id;
+    @ColumnInfo
+    private String english;
+    @ColumnInfo
+    private String means;
+    @ColumnInfo
+    private long timestamp;
 
     public Vocabulary(String english, String means, long timestamp) {
-        this.mEnglish = english;
-        this.mMeans = means;
-        this.mTimestamp = timestamp;
+        this.english = english;
+        this.means = means;
+        this.timestamp = timestamp;
     }
 
     public Vocabulary() {
 
     }
 
-    public String getEnglish() {
-        return mEnglish;
-    }
-
-    public void setEnglish(String english) {
-        this.mEnglish = english;
-    }
-
-    public String getMeans() {
-        return mMeans;
-    }
-
-    public void setMeans(String means) {
-        this.mMeans = means;
-    }
-
-    public long getTimestamp() {
-        return mTimestamp;
-    }
-
-    public void setTimestamp(long timestamp) {
-        this.mTimestamp = timestamp;
-    }
-
     public int getId() {
-        return mId;
+        return id;
     }
 
     public void setId(int id) {
-        this.mId = id;
+        this.id = id;
+    }
+
+    public String getEnglish() {
+        return english;
+    }
+
+    public void setEnglish(String english) {
+        this.english = english;
+    }
+
+    public String getMeans() {
+        return means;
+    }
+
+    public void setMeans(String means) {
+        this.means = means;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
     }
 }
