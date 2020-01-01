b0a51edf71a65c669d57e18df798effa88c3bbb7
Yebonkim <v954242@gmail.com>
2020-01-01 21:45:56 +0900

    Add getting voca cnt in MainActivity by Room DB

--- app/src/main/java/example/com/englishnote/MainActivity.java
+++ app/src/main/java/example/com/englishnote/MainActivity.java
@@ -5,40 +5,73 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import example.com.englishnote.database.AppDatabase;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+    private int mVocaCnt = -1;
+
+    private AppDatabase mDb;
+    private Disposable mVocaCntDisposable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        mDb = Room.databaseBuilder(getApplicationContext(), AppDatabase.class,
+                AppDatabase.DATABASE_NAME).build();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        subscribeVocaCnt();
+    }
+
+    protected void subscribeVocaCnt() {
+        mVocaCntDisposable = mDb.vocaDao().getCount().observeOn(Schedulers.io())
+                .subscribeOn(Schedulers.computation())
+                .subscribe(integer -> {
+                    mVocaCnt = integer.intValue();
+                });
     }
 
     @OnClick(R.id.button_study)
     public void onClickStudyBtn() {
-//        if (new VocabularyDBDAO(this).getCount() == 0) {
-//            Toast.makeText(this, getString(R.string.error_limit_1_word), Toast.LENGTH_SHORT).show();
-//            return;
-//        }
-//        startActivity(new Intent(this, StudyActivity.class));
+        if (mVocaCnt == 0) {
+            Toast.makeText(this,
+                    getString(R.string.error_limit_1_word), Toast.LENGTH_SHORT).show();
+            return;
+        }
+        startActivity(new Intent(this, StudyActivity.class));
     }
 
     @OnClick(R.id.button_test)
     public void onClickTestBtn() {
-//        if (new VocabularyDBDAO(this).getCount() < 4) {
-//            Toast.makeText(this, getString(R.string.error_limit_4_word), Toast.LENGTH_SHORT).show();
-//            return;
-//        }
-//        startActivity(new Intent(this, TestActivity.class));
+        if (mVocaCnt < 4) {
+            Toast.makeText(this,
+                    getString(R.string.error_limit_4_word), Toast.LENGTH_SHORT).show();
+            return;
+        }
+        startActivity(new Intent(this, TestActivity.class));
     }
 
     @OnClick(R.id.button_register)
     public void onClickRegisterBtn() {
         startActivity(new Intent(this, VocaListActivity.class));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mVocaCntDisposable.dispose();
     }
 
     @Override
