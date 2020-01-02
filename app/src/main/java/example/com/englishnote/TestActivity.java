43fc2f2220ca02b916d00df70797163fefc52dde
Yebonkim <v954242@gmail.com>
2020-01-02 20:52:25 +0900

    Update TestActivity to using Room DB
    - Update TestActivity to singleTop for retest

--- app/src/main/java/example/com/englishnote/TestActivity.java
+++ app/src/main/java/example/com/englishnote/TestActivity.java
@@ -12,19 +12,30 @@ import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.room.Room;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.BindViews;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import example.com.englishnote.common.IntentExtra;
+import example.com.englishnote.database.AppDatabase;
 import example.com.englishnote.model.Vocabulary;
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class TestActivity extends AppCompatActivity {
+    private final static int ANSWER_CNT_MAX = 4;
+    private final static int TIME_OF_JUDGMENT_SHOWING = 1000;
+
     @BindView(R.id.toolbar)
     Toolbar toolbar;
     @BindView(R.id.text_english)
@@ -32,15 +43,17 @@ public class TestActivity extends AppCompatActivity {
     @BindViews({R.id.button_means1, R.id.button_means2, R.id.button_means3, R.id.button_means4})
     List<Button> meansBtns;
 
-    private final static int ANSWER_CNT_MAX = 4;
-    private final static int TIME_OF_JUDGMENT_SHOWING = 2000;
+    private AppDatabase mDb;
+    private Disposable mVocaListDisposable;
 
-    private List<Vocabulary> mData;
+    private List<Vocabulary> mTestData;
     private ArrayList<Integer> mWrongVocaIds;
-    private List<Integer> mAnswerIdList;
+    private Map<Integer, String> mAnswerIdMeanMap = new HashMap<>();
     private int mNowQuestions = -1;
 
-    private Handler mHandler;
+    private Handler mHandler = new Handler();
+
+    private AlertDialog.Builder mDialogBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,46 +62,49 @@ public class TestActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         ActionBarManager.initBackArrowActionbar(this, toolbar, getString(R.string.action_test));
-        getData();
-        init();
+
+        mDb = Room.databaseBuilder(getApplicationContext(), AppDatabase.class,
+                AppDatabase.DATABASE_NAME).build();
+        mVocaListDisposable = mDb.vocaDao().selectAll().observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(vocaList -> {
+                    mTestData = vocaList;
+                    for (Vocabulary voca : mTestData) {
+                        mAnswerIdMeanMap.put(voca.getId(), voca.getMeans());
+                    }
+                    Collections.shuffle(mTestData);
+                    startTest();
+                });
+        setUpDialog();
+    }
+
+    protected void startTest() {
+        mNowQuestions = -1;
+        mWrongVocaIds = new ArrayList<>();
         mHandler.post(mSettingNewQuestionRunnable);
     }
 
-    private void init() {
-        mHandler = new Handler();
-        mWrongVocaIds = new ArrayList<>();
-        mAnswerIdList = new ArrayList<>();
-//        List<Vocabulary> originalData = mDb.selectAll();
-//
-//        for(int i = 0; i < originalData.size(); i++) {
-//            mAnswerIdList.add(originalData.get(i).getId());
-//        }
-    }
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        mWrongVocaIds = intent.getIntegerArrayListExtra(IntentExtra.VOCA_ID_LIST);
+        mTestData = new ArrayList<>();
 
-    protected void getData() {
-        mWrongVocaIds = getIntent().getIntegerArrayListExtra(IntentExtra.VOCA_ID_LIST);
-//        mDb = new VocabularyDBDAO(this);
-        Vocabulary voca;
+        mVocaListDisposable = Observable.fromArray(mWrongVocaIds.toArray())
+                .flatMap(id -> mDb.vocaDao().findById((int) id))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(voca -> {
+                    mTestData.add(voca);
 
-//        if (mWrongVocaIds == null) {
-//            mData = mDb.selectAll();
-//        } else {
-//            mData = new ArrayList<>();
-//
-//            for (int id : mWrongVocaIds) {
-//                voca = mDb.selectById(id);
-//
-//                if (voca != null) {
-//                    mData.add(voca);
-//                }
-//            }
-//        }
-//
-//        Collections.shuffle(mData);
+                    if (mTestData.size() == mWrongVocaIds.size()) {
+                        startTest();
+                    }
+                });
     }
 
     protected boolean isCorrect(String clickedMeans) {
-        if (mData.get(mNowQuestions).getMeans().equals(clickedMeans)) {
+        if (mTestData.get(mNowQuestions).getMeans().equals(clickedMeans)) {
             return true;
         } else {
             return false;
@@ -96,38 +112,45 @@ public class TestActivity extends AppCompatActivity {
     }
 
     protected boolean isEnd() {
-        return (mNowQuestions == mData.size() - 1) ? true : false;
+        return (mNowQuestions == mTestData.size() - 1) ? true : false;
     }
 
-    Runnable mSettingNewQuestionRunnable = new Runnable() {
+    private Runnable mSettingNewQuestionRunnable = new Runnable() {
+        List<String> mAnswers = new ArrayList<>();
+        List<Integer> mVocaIdSet;
+
         @Override
         public void run() {
             mNowQuestions++;
-//            englishText.setText(mData.get(mNowQuestions).getEnglish());
-//
-//            List<String> answers = new ArrayList<>();
-//
-//            answers.add(mData.get(mNowQuestions).getMeans());
-//            Collections.shuffle(mAnswerIdList);
-//
-//            int idx=0;
-//            Vocabulary voca;
-//            for(int cnt = 0 ; cnt < ANSWER_CNT_MAX - 1 ; cnt++) {
-//                if (mAnswerIdList.get(idx) == mData.get(mNowQuestions).getId()) {
-//                    idx++;
-//                }
-//                voca = mDb.selectById(mAnswerIdList.get(idx++));
-//
-//                if (voca != null) {
-//                    answers.add(voca.getMeans());
-//                }
-//            }
-//            Collections.shuffle(answers);
-//
-//            for(int i = 0 ; i < ANSWER_CNT_MAX ; i++) {
-//                meansBtns.get(i).setText(answers.get(i));
-//            }
-//            changeButtonEnables(true);
+            englishText.setText(mTestData.get(mNowQuestions).getEnglish());
+
+            setNewAnswers(mTestData.get(mNowQuestions).getMeans());
+
+            setMeanTextsOnBtns();
+            changeButtonEnables(true);
+        }
+
+        private void setNewAnswers(String realAnswer) {
+            mAnswers.clear();
+            mAnswers.add(realAnswer);
+
+            mVocaIdSet = new ArrayList<>(mAnswerIdMeanMap.keySet());
+            Collections.shuffle(mVocaIdSet);
+
+            int id = 1;
+            for (int cnt = 0 ; cnt < ANSWER_CNT_MAX - 1 ; cnt++) {
+                if (mTestData.get(mNowQuestions).getId() == id) {
+                    id++;
+                }
+                mAnswers.add(mAnswerIdMeanMap.get(id++));
+            }
+            Collections.shuffle(mAnswers);
+        }
+
+        private void setMeanTextsOnBtns() {
+            for (int i = 0 ; i < ANSWER_CNT_MAX ; i++) {
+                meansBtns.get(i).setText(mAnswers.get(i));
+            }
         }
     };
 
@@ -138,12 +161,11 @@ public class TestActivity extends AppCompatActivity {
     protected void goToTestActivityWithWrongWord() {
         startActivity(new Intent(this, TestActivity.class)
                 .putIntegerArrayListExtra(IntentExtra.VOCA_ID_LIST, mWrongVocaIds));
-        finish();
     }
 
-    protected void popUpDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(getString(R.string.msg_test_again_wrong_voca))
+    private void setUpDialog() {
+        mDialogBuilder = new AlertDialog.Builder(this);
+        mDialogBuilder.setMessage(getString(R.string.msg_test_again_wrong_voca))
                 .setTitle(R.string.action_test)
                 .setCancelable(true)
                 .setPositiveButton(getString(R.string.action_yes), new DialogInterface.OnClickListener() {
@@ -159,35 +181,45 @@ public class TestActivity extends AppCompatActivity {
                         goToMainActivity();
                     }
                 });
-        builder.show();
+    }
+
+    private void popUpDialog() {
+        mDialogBuilder.show();
     }
 
     protected void changeButtonEnables(boolean isEnable) {
-        for(Button btn : meansBtns) {
+        for (Button btn : meansBtns) {
             btn.setEnabled(isEnable);
         }
     }
 
     @OnClick({R.id.button_means1, R.id.button_means2, R.id.button_means3, R.id.button_means4})
     public void onMeansBtnsClicked(Button button) {
-//        changeButtonEnables(false);
-//
-//        if(isCorrect(button.getText().toString())) {
-//            englishText.setText(getString(R.string.msg_correct));
-//        } else {
-//            englishText.setText(getString(R.string.msg_wrong));
-//            mWrongVocaIds.add(mData.get(mNowQuestions).getId());
-//        }
-//
-//        if(isEnd()) {
-//            if (mWrongVocaIds.size() == 0) {
-//                Toast.makeText(TestActivity.this, getString(R.string.msg_all_solved), Toast.LENGTH_SHORT).show();
-//                goToMainActivity();
-//            } else {
-//                popUpDialog();
-//            }
-//        } else
-//            mHandler.postDelayed(mSettingNewQuestionRunnable, TIME_OF_JUDGMENT_SHOWING);
+        changeButtonEnables(false);
+
+        if(isCorrect(button.getText().toString())) {
+            englishText.setText(getString(R.string.msg_correct));
+        } else {
+            englishText.setText(getString(R.string.msg_wrong));
+            mWrongVocaIds.add(mTestData.get(mNowQuestions).getId());
+        }
+
+        if(isEnd()) {
+            if (mWrongVocaIds.size() == 0) {
+                Toast.makeText(TestActivity.this, getString(R.string.msg_all_solved), Toast.LENGTH_SHORT).show();
+                goToMainActivity();
+            } else {
+                popUpDialog();
+            }
+        } else {
+            mHandler.postDelayed(mSettingNewQuestionRunnable, TIME_OF_JUDGMENT_SHOWING);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mVocaListDisposable.dispose();
     }
 
     @Override
