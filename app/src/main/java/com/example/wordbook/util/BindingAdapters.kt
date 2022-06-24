87e560886815862da923b643119392a1d85c60c7
yebonkim <v954242@gmail.com>
2022-06-24 23:20:21 +0900

    Add study feature

--- app/src/main/java/com/example/wordbook/util/BindingAdapters.kt
+++ app/src/main/java/com/example/wordbook/util/BindingAdapters.kt
@@ -1,26 +1,62 @@
 package com.example.wordbook.util
 
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.example.wordbook.R
 import com.example.wordbook.database.Word
+import com.example.wordbook.study.StudyViewModel
 import com.example.wordbook.test.TestViewModel
 
 @BindingAdapter("english")
-fun setEnglish(textView: TextView, word: Word) {
-    textView.text = word.english
+fun TextView.setEnglish(word: Word?) {
+    text = word?.let {
+        it.english
+    } ?: ""
 }
 
 @BindingAdapter("means")
-fun setMeans(textView: TextView, word: Word) {
-    textView.text = word.means
+fun TextView.setMeans(word: Word?) {
+    text = word?.let {
+        it.means
+    } ?: ""
+}
+
+@BindingAdapter("studyEnglishVisibility")
+fun TextView.setStudyEnglishVisibility(studyType: StudyViewModel.StudyType) {
+    visibility = when (studyType) {
+        StudyViewModel.StudyType.BOTH -> View.VISIBLE
+        StudyViewModel.StudyType.ONLY_ENGLISH -> View.VISIBLE
+        StudyViewModel.StudyType.ONLY_KOREAN -> View.GONE
+    }
+}
+
+@BindingAdapter("studyMeansVisibility")
+fun TextView.setStudyMeansVisibility(studyType: StudyViewModel.StudyType) {
+    visibility = when (studyType) {
+        StudyViewModel.StudyType.BOTH -> View.VISIBLE
+        StudyViewModel.StudyType.ONLY_ENGLISH -> View.GONE
+        StudyViewModel.StudyType.ONLY_KOREAN -> View.VISIBLE
+    }
+}
+
+@BindingAdapter("studyTypeButtonText")
+fun Button.setStudyTypeButtonText(studyType: StudyViewModel.StudyType) {
+    text = context.getString(
+        when (studyType) {
+            StudyViewModel.StudyType.BOTH -> R.string.title_both
+            StudyViewModel.StudyType.ONLY_ENGLISH -> R.string.title_only_english
+            StudyViewModel.StudyType.ONLY_KOREAN -> R.string.title_only_korean
+        }
+    )
 }
 
 @BindingAdapter("question")
-fun TextView.setWordEnglish(item: TestViewModel.TestUnit?) {
-    item?.let {
-        text =  it.question.english
-    }
+fun TextView.setQuestionEnglish(item: TestViewModel.TestUnit?) {
+    text = item?.let {
+        it.question.english
+    } ?: ""
 }
 
 @BindingAdapter("resultState")
